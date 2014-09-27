2e410e4c1462cc0e618a49bcb94958bee3d90ce2
Juan Pedro Lozano <jp@yreaction.com>
2014-09-27 13:39:10 +0200

    WEATHER DAY 2

---
+++ ios8/adaptativeWeather/adaptativeWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  adaptativeWeather
+//
+//  Created by yreaction on 24/9/14.
+//  Copyright (c) 2014 yreaction. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ playgrounds/CHAPTER2.playground/section-1.swift
@@ -0,0 +1,5 @@
+
+var anArray:[Int] = [1,2,3,4,5]
+anArray.extend(5...9)
+println(anArray)
+
